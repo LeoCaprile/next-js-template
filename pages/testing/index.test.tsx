372745cfe3bcf94e5406446a587ebab531480fe2
Matías Fuentes <mfuentes@jumpitt.com>
2023-01-04 19:52:50 -0300

    :white_check_mark: (Testing): Update test to be better examples
    Updated one test and create another to give more examples on how to test a component

--- pages/testing/index.test.tsx
+++ pages/testing/index.test.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import TestingPage, { getServerSideProps } from '.';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 describe('Testing the testing page', () => {
-  const text = 'Hello World!';
+  const MoocText = 'Hello World!';
 
   it('getServerSideProps return the correct text "HelloWorld!"', async () => {
     // Arrange
+
     const response = await getServerSideProps();
-    const expectedResponse = { props: { text } };
+    const expectedResponse = { props: { text: MoocText } };
     //Act
     // * Nothing because this is a test example
     //Assert
     expect(response).toEqual(expectedResponse);
   });
 
-  it('Component is rendering', () => {
-    const component = render(<TestingPage text={text} />);
+  it('Render the component and have a header', () => {
+    //Arrange
+    render(<TestingPage text={MoocText} />);
+    const headerText = screen.getByRole('heading', { level: 1 }).textContent;
+    //Assert
+    expect(headerText).toBe('Testing on this template!');
+  });
+
+  it('Catch check if the props are rendered', () => {
+    //Arrange
+    render(<TestingPage text={MoocText} />);
+    const moocTextRendered = screen.getByRole('note').textContent;
+    //Assert
+    expect(moocTextRendered).toBe('(' + MoocText + ')');
   });
 });
--- pages/testing/index.tsx
+++ pages/testing/index.tsx
@@ -9,7 +9,9 @@ export default function TestingPage({ text }: PageProps) {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Testing on this template!</h1>
-        <p>This ({text})is being server side rendered</p>
+        <p>
+          This <small role="note">({text})</small>is being server side rendered
+        </p>
         <p>See the code to know how to test page components</p>
       </main>
     </div>
